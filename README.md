92493749bb54e89688ca4c451294b7e64fc22485
MJaroslav <rcr3211@gmail.com>
2017-12-27 17:31:44 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# KurisuBot
+
+Versatile Bot for Discord and VK.
+
+(Created and rewritten for personal use)
---
+++ src/main/java/mjaroslav/bots/kurisu/KurisuBot.java
@@ -0,0 +1,7 @@
+package mjaroslav.bots.kurisu;
+
+public class KurisuBot {
+	public static void main(String... args) {
+
+	}
+}
